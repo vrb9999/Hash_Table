20cf53f1be32c27e3c41f9c84a51d694105f71e7
vrb9999 <vbiradar9999@gmail.com>
2022-05-14 01:28:38 +0530

    UC-1 Day-15 Find the frequency of word in sentence

---
+++ Find_Frequency/Find_Frequency/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Find_Frequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			MapNode<String, int> myHashMap = new MapNode<String, int>(5);
+			String sentence = "to be or not to be";
+			String[] words = sentence.ToLower().Split(" ");
+			foreach (String word in words)
+			{
+				Console.WriteLine(word + " --> Added to HashTable");
+				int value = myHashMap.Get(word);
+				if (value == 0)
+					value = 1;
+				else
+					value = value + 1;
+				myHashMap.Add(word, value);
+			}
+			int frequency = myHashMap.Get("be");
+			Console.WriteLine("Frequency of word -> be -> " +frequency);
+		}
+    }
+}
