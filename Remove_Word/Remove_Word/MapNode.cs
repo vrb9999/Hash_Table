b923fbde37c3900f54942566a0202a65ce7b9388
vrb9999 <vbiradar9999@gmail.com>
2022-05-14 01:32:05 +0530

    UC-3 Day-15 Remove word from paragraph

---
+++ Remove_Word/Remove_Word/MapNode.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remove_Word
+{
+    class MapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public MapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddFirst(item);
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        public int Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    string s = item.Value.ToString();
+                    return int.Parse(s);
+                }
+            }
+            return 0;
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                Console.WriteLine(key + "-->Removed");
+                linkedList.Remove(foundItem);
+            }
+        }
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+    }
+}
---
+++ Remove_Word/Remove_Word/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Remove_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MapNode<String, int> myHashMap = new MapNode<String, int>(5);
+            String sentence = "Paranoids are not paranoid because they are paranoid but because they " +
+                "keep putting themselves deliberately into paranoid avoidable situations";
+            String[] words = sentence.ToLower().Split(" ");
+            foreach (String word in words)
+            {
+                Console.WriteLine(word + " --> Added to HashTable");
+                int value = myHashMap.Get(word);
+                if (value == 0)
+                    value = 1;
+                else
+                    value = value + 1;
+                myHashMap.Add(word, value);
+            }
+            int frequency = myHashMap.Get("paranoid");
+            Console.WriteLine("Frequency of word -->paranoid --> " + frequency);
+            myHashMap.Remove("avoidable");
+        }
+    }
+}
